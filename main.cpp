8dc517adc4c21c4f20615da27046990ddcbc624d
Andrew Woo <seungminleader@gmail.com>
2020-11-03 19:19:19 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
+
+#include <ASWL/root/include/utilities.hpp>
 
 int main() {
 
     std::cout << "Hello World!" << std::endl;
+    ASWL::utilities::Logger("Hello World", false);
 }
